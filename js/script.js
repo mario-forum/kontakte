b93015490326e49ddba18e77811c61f5a40de304
Mario - Forum <mario.soardo@forum-berufsbildung.de>
2022-01-11 16:23:02 +0100

    add input empty validation & css

--- js/script.js
+++ js/script.js
@@ -15,6 +15,49 @@ const email = document.getElementById('email');
 
 // Eventlistener für Formular
 form.addEventListener('submit', function (e) {
+    // Form submit unterbinden
     e.preventDefault('submit');
-    console.log(e);
+
+    // Funktionsaufruf, erhält ein Array als Parameter
+    // Call pass an array as parameter
+    checkInputsNotEmpty([firstname, lastname, phone, email]) // checkInputsNichtLeer()
 });
+
+// FUNKTIONEN
+// FUNCTIONS
+function checkInputsNotEmpty(domArray) {
+    // console.log(domArray);
+    domArray.forEach( (input) => {
+        // console.log(input.value);
+        if (input.value.trim() === "") {
+            // Funktionsaufruf Fehlermeldung
+            checkFehler(input, `${getInputName(input)} ist erforderlich`);
+        } else {
+            // Funktionsaufruf Erfolgreich
+            checkOK(input)
+        }
+    });
+}
+
+// Gibt die ID des Input-Elements zurück
+// Returns id of input element
+function getInputName(input) {
+    return input.id;
+}
+
+// Input leer Funktion
+// Input empty
+function checkFehler(input, message) {
+    const formInput = input.parentElement;
+    formInput.className = 'form-input error';
+    const small = document.querySelector('small');
+    small.innerText = message;
+}
+
+// Input nicht leer Funktion
+// Input not empty Function
+function checkOK(input) {
+    const formInput = input.parentElement;
+    formInput.className = 'form-input success'
+    // formInput.classList.add('success'); // Füge neuen css-classname zusätzlich zu
+}
